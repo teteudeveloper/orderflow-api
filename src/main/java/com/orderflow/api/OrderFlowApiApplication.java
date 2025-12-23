768279325cb58f70ed0dedaf2b04701c6d1920a3
teteudeveloper <teteudeveloper@gmail.com>
2025-12-23 11:00:34 -0300

    chore: add main application class and gitignore

---
+++ src/main/java/com/orderflow/api/OrderFlowApiApplication.java
@@ -0,0 +1,12 @@
+package com.orderflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFlowApiApplication.class, args);
+    }
+}
